85b6f5c3d41b91dda301fa34de1a5c2e302340d7
rmnas000 <rmanas000@gmail.com>
2020-03-10 15:44:27 +0530

    amazoniee 1st commit

---
+++ models/review.js
@@ -0,0 +1,20 @@
+var mongoose = require("mongoose");
+var Schema=mongoose.Schema
+viewSchema= new Schema(
+    {
+        name:{
+            type:String,
+            required:true
+        },
+        review:{
+            type:String,
+            required:true
+        }
+    }
+)
+
+
+var review=mongoose.model("review",viewSchema)
+
+
+module.exports=review
---
+++ server.js
@@ -0,0 +1,53 @@
+var express=require("express")
+var session=require("express-session")
+var path=require("path")
+
+var hbs = require("hbs")
+var dotenv = require("dotenv")
+dotenv.config();
+require("./db")
+
+// var methodOverride=require("method-override")
+var app=express()
+var productNormalroute=require("./routes/productNormalroutes")
+var userNormalroute=require("./routes/userNormalroutes")
+var userApiroute=require("./routes/userApiroutes")
+var productApiroute=require("./routes/productApiroutes")
+
+
+
+
+
+
+app.use(express.json())
+app.use(express.urlencoded({extended:false}))
+app.set("view engine","hbs")
+app.set("view options",{layout:"layout"})
+// app.use(methodOverride("logout"))
+app.use(express.static(path.join(__dirname,"static")))
+hbs.registerPartials(path.join(__dirname,"views","partials"))
+app.use(
+    session({
+      secret: "todosAPIexpressappsecret",
+      resave: false,
+      name: "todoSession", 
+      saveUninitialized: false,
+      cookie: {
+        maxAge: 1000 * 60 * 30,
+        httpOnly: true,
+        secure: false,
+        sameSite: "strict"
+      }
+    })
+  );
+
+
+  app.use(userApiroute)
+  app.use(userNormalroute)
+  app.use(productNormalroute)
+  app.use(productApiroute)
+  app.listen(5555,function(){
+    console.log("session loading...")
+});
+
+
